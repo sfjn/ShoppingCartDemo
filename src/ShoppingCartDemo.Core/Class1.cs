cb2776b6a7b04f639dc572f055e0d845b2f13f16
Johan Nordström <lepthone10@gmail.com>
2017-02-06 17:20:09 +0100

    added core project for logging etc

---
+++ src/ShoppingCartDemo.Core/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCartDemo.Core
+{
+    public class Class1
+    {
+    }
+}
